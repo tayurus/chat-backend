9bcff4a456ea28408d823c764ef8110ce949ce75
y.tatarintsev <y.tatarintsev@corp.vk.com>
2022-09-05 21:32:59 +0500

    backend - тестирование

--- src/tests/controllers/dialogs/getDialogs.test.ts
+++ src/tests/controllers/dialogs/getDialogs.test.ts
@@ -1,18 +1,17 @@
-import { clearDB, connectToDB, disconnectFromDB } from '../config/database';
-import { WebSocketModule } from '../utils/websocketModule';
-import { REGISTER_SUCCESS_INPUT_DATA, REGISTER_SUCCESS_INPUT_DATA2, registerUserForTest } from './helpers';
+import { clearDB, connectToDB, disconnectFromDB } from '../../../config/database';
+import { WebSocketModule } from '../../../utils/websocketModule';
+import { REGISTER_SUCCESS_INPUT_DATA, REGISTER_SUCCESS_INPUT_DATA2, registerUserForTest } from '../../helpers';
 import { describe } from '@jest/globals';
 import request from 'supertest';
-import { app } from '../app';
-import { BASE_ROUTES, USER_ROUTES } from '../types/backendAndFrontendCommonTypes/routes';
-import { RegisteredUserForTest } from './helpers';
+import { app } from '../../../app';
+import { BASE_ROUTES, USER_ROUTES } from '../../../types/backendAndFrontendCommonTypes/routes';
 
-let registeredUsers: Record<string, RegisteredUserForTest> = {};
+let authTokens: Record<string, string> = {};
 
 beforeAll(async () => await connectToDB());
 beforeEach(done => {
   registerUserForTest([REGISTER_SUCCESS_INPUT_DATA, REGISTER_SUCCESS_INPUT_DATA2]).then(authTokensFromBackend => {
-    registeredUsers = Object.assign({}, authTokensFromBackend);
+    authTokens = Object.assign({}, authTokensFromBackend);
     WebSocketModule.server.close(() => done());
   });
 });
@@ -25,7 +24,7 @@ describe('Пример теста', () => {
   test('Выполняет поиск пользователей', done => {
     request(app)
       .get(`${BASE_ROUTES.USER}${USER_ROUTES.SEARCH_USERS}?query=${REGISTER_SUCCESS_INPUT_DATA.first_name}`)
-      .set('Cookie', [`token=${registeredUsers[REGISTER_SUCCESS_INPUT_DATA.email]}`])
+      .set('Cookie', [`token=${authTokens[REGISTER_SUCCESS_INPUT_DATA.email]}`])
       .send()
       .expect(200)
       .end((err, res) => {
